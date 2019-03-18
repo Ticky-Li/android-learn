c2163e39dd6441e464164026382c77331e3ff2aa
Tikee_Li <tikee_li@163.com>
2019-03-18 16:08:26 +0800

    更新 及上传 pdf 资料

--- gen/com/jarppy/example/activitytest/R.java
+++ gen/com/jarppy/example/activitytest/R.java
@@ -14,12 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f050001;
-        public static final int button1=0x7f050000;
+        public static final int LinearLayout1=0x7f060001;
+        public static final int add_item=0x7f060002;
+        public static final int button_1=0x7f060000;
+        public static final int remove_item=0x7f060003;
     }
     public static final class layout {
         public static final int first_layout=0x7f030000;
         public static final int main=0x7f030001;
+    }
+    public static final class menu {
+        public static final int main=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/com/jarppy/example/activitytest/FirstActivity.java
+++ src/com/jarppy/example/activitytest/FirstActivity.java
@@ -1,9 +1,9 @@
 package com.jarppy.example.activitytest;
 
-import com.jarppy.example.helloworld.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -18,12 +18,36 @@ public class FirstActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE); // 活动中不显示标题栏
 		setContentView(R.layout.first_layout);
 		
-		Button button1 = (Button) findViewById(R.id.button1);
-		button1.setOnClickListener(new OnClickListener() { // 监听 id=button1,按钮的点击事件
+		Button button1 = (Button) findViewById(R.id.button_1);
+		button1.setOnClickListener(new OnClickListener() { // 调用 setOnClickListener()方法为按钮注册一个监听器，点击按钮时就会执行监听器中的 onClick()方法。
 			public void onClick(View v) {
 				Toast.makeText(FirstActivity.this, "You clicked Button 1",
 						Toast.LENGTH_SHORT).show(); // 显示 点击后的toast提示消息
 			}
 		});
+	}
+	
+	/**
+	 * 重写onCreateOptionsMenu()
+	 */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu); //  getMenuInflater()方法能够得到 MenuInflater对象，再调用它的 inflate()方法就可以给当前活动创建菜单了
+		return true; // 返回 true，表示允许创建的菜单显示出来，如果返回了 false，创建的菜单将无法显示。
+	}
+	
+	/**
+	 * 重写onOptionsItemSelected()方法
+	 */
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) { //  item.getItemId()来判断我们点击的是哪一个菜单项
+			case R.id.add_item:
+				Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.remove_item:
+				Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+				break;
+			default:
+		}
+		return true;
 	}
 }
